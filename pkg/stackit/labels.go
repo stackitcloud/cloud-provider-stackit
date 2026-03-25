00dbcd30044b1e03a435d98cdc9071c8bbdeccbd
stackit-ske <89182727+stackit-ske@users.noreply.github.com>
2026-03-25 10:06:13 +0100

    [release-v1.35] update go to 1.26.1 (#913)
    * update go to 1.26.1
    
    Signed-off-by: Felix Breuer <f.breuer94@gmail.com>
    
    * fix atomic uint64
    
    Signed-off-by: Felix Breuer <f.breuer94@gmail.com>
    
    ---------
    
    Signed-off-by: Felix Breuer <f.breuer94@gmail.com>
    Co-authored-by: Felix Breuer <f.breuer94@gmail.com>

--- pkg/stackit/labels.go
+++ pkg/stackit/labels.go
@@ -1,12 +1,12 @@
 package stackit
 
-type labels map[string]interface{}
+type labels map[string]any
 
 func labelsFromTags(tags map[string]string) labels {
-	// Create a new map of type map[string]interface{}
-	l := make(map[string]interface{}, len(tags))
+	// Create a new map of type map[string]any
+	l := make(map[string]any, len(tags))
 
-	// Convert each value from string to interface{}
+	// Convert each value from string to any
 	for key, value := range tags {
 		l[key] = value
 	}
