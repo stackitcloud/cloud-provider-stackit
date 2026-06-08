b0bc5041dde3c6f74a7513159a929553d924b0b2
Björn Fischer <bjoern.fischer@stackit.cloud>
2026-06-08 18:29:46 +0200

    Remove license headers

--- pkg/alb/ingress/ingressclass_controller.go
+++ pkg/alb/ingress/ingressclass_controller.go
@@ -1,26 +1,10 @@
-/*
-Copyright 2025.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package ingress
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"time"
-	"errors"
 
 	"github.com/stackitcloud/cloud-provider-stackit/pkg/stackit"
 	stackitconfig "github.com/stackitcloud/cloud-provider-stackit/pkg/stackit/config"
@@ -101,14 +85,14 @@ func (r *IngressClassReconciler) Reconcile(ctx context.Context, req ctrl.Request
 	}
 
 	for _, errItem := range errorList {
-        var evtErr *errorEvent
-        
-        if errors.As(errItem, &evtErr) {
-            log.Info(evtErr.description, "typ", evtErr.typ, "ingressRef", evtErr.ingressRef)
-        } else {
-            log.Info(errItem.Error())
-        }
-    }
+		var evtErr *errorEvent
+
+		if errors.As(errItem, &evtErr) {
+			log.Info(evtErr.description, "typ", evtErr.typ, "ingressRef", evtErr.ingressRef)
+		} else {
+			log.Info(errItem.Error())
+		}
+	}
 	r.SendEvents(ingressClass, errorList)
 
 	requeue, err := r.updateStatus(ctx, ingressClass)
