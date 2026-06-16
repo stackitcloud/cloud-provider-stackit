8e3152bdbf8097f7dba8345be9d62fae40a8855e
stackit-ske <89182727+stackit-ske@users.noreply.github.com>
2026-06-16 16:59:17 +0200

    [release-v1.35] Add support for MutableCSINodeAllocatableCount (#1231)
    * Add support for MutableCSINodeAllocatableCount
    
    The CSI list's all PCIe devices that are not of
    type VIRTIO_BLOCK_DEVICE and subtracts them from
    the theoretically maximum, so kubernetes can report
    a correct dynamic max volume count that can be attached
    for each node.
    
    Signed-off-by: Niclas Schad <niclas.schad@stackit.cloud>
    
    * fix imports for linux
    
    Signed-off-by: Niclas Schad <niclas.schad@stackit.cloud>
    
    * subtract one from maxVolumes for root partition
    
    Signed-off-by: Niclas Schad <niclas.schad@stackit.cloud>
    
    * WIP
    
    Signed-off-by: Niclas Schad <niclas.schad@stackit.cloud>
    
    * parse Body instead of ErrorMessage field in IsTooManyDevicesError()
    
    Signed-off-by: Niclas Schad <niclas.schad@stackit.cloud>
    
    * cleanup
    
    Signed-off-by: Niclas Schad <niclas.schad@stackit.cloud>
    
    * address linter feedback
    
    Signed-off-by: Niclas Schad <niclas.schad@stackit.cloud>
    
    * add unit tests and restructure into helper function
    
    Signed-off-by: Felix Breuer <f.breuer94@gmail.com>
    
    * rewrite tests with ginkgo
    
    Signed-off-by: Felix Breuer <f.breuer94@gmail.com>
    
    * fix golint
    
    Signed-off-by: Felix Breuer <f.breuer94@gmail.com>
    
    * add volumeDevice counting
    
    * some adjustments
    
    ---------
    
    Signed-off-by: Niclas Schad <niclas.schad@stackit.cloud>
    Signed-off-by: Felix Breuer <f.breuer94@gmail.com>
    Co-authored-by: Niclas Schad <niclas.schad@stackit.cloud>
    Co-authored-by: Felix Breuer <f.breuer94@gmail.com>

---
+++ pkg/csi/util/mount/mount_helper.go
@@ -0,0 +1,121 @@
+package mount
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"k8s.io/klog/v2"
+)
+
+var (
+	// pciClassBridgePCI matches the Linux PCI-to-PCI bridge class prefix.
+	pciClassBridgePCI = []byte("0x0604")
+)
+
+const (
+	globalMountDir   = "globalmount"
+	volumeDevicesDir = "volumeDevices"
+	volumeDataDir    = "data"
+	volumeDataFile   = "vol_data.json"
+	driverNameKey    = "driverName"
+)
+
+func countFreePCIeSlotsAt(devicesPath string) (int64, error) {
+	devices, err := os.ReadDir(devicesPath)
+	if err != nil {
+		return 0, fmt.Errorf("failed to read PCI bus: %w", err)
+	}
+
+	var freePCIeSlots int64
+
+	for _, dev := range devices {
+		devPath := filepath.Join(devicesPath, dev.Name())
+
+		classBuf, err := os.ReadFile(filepath.Join(devPath, "class"))
+		if err != nil {
+			klog.Errorf("failed to read PCI device class %s: %v", devPath, err)
+			continue
+		}
+
+		if !bytes.HasPrefix(bytes.TrimSpace(classBuf), pciClassBridgePCI) {
+			continue
+		}
+
+		children, err := filepath.Glob(filepath.Join(devPath, "????:??:??.?"))
+		if err != nil {
+			klog.Errorf("failed to glob PCI children for %s: %v", devPath, err)
+			continue
+		}
+
+		if len(children) == 0 {
+			freePCIeSlots++
+		}
+	}
+
+	return freePCIeSlots, nil
+}
+
+func countLocalCSIVolumesAt(csiPluginDir, driverName string) (int64, error) {
+	driverPluginDir := filepath.Join(csiPluginDir, driverName)
+
+	filesystemVolumes, err := countLocalCSIFilesystemVolumesAt(driverPluginDir)
+	if err != nil {
+		return 0, err
+	}
+
+	blockVolumes, err := countLocalCSIBlockVolumesAt(csiPluginDir, driverName)
+	if err != nil {
+		return 0, err
+	}
+
+	return filesystemVolumes + blockVolumes, nil
+}
+
+func countLocalCSIFilesystemVolumesAt(driverPluginDir string) (int64, error) {
+	volumeMounts, err := filepath.Glob(filepath.Join(driverPluginDir, "*", globalMountDir))
+	if err != nil {
+		return 0, fmt.Errorf("failed to glob CSI volume mounts in %s: %w", driverPluginDir, err)
+	}
+
+	return int64(len(volumeMounts)), nil
+}
+
+func countLocalCSIBlockVolumesAt(csiPluginDir, driverName string) (int64, error) {
+	volumeMetadataFiles, err := filepath.Glob(filepath.Join(csiPluginDir, volumeDevicesDir, "*", volumeDataDir, volumeDataFile))
+	if err != nil {
+		return 0, fmt.Errorf("failed to glob CSI block volume metadata in %s: %w", csiPluginDir, err)
+	}
+
+	var blockVolumes int64
+
+	for _, metadataPath := range volumeMetadataFiles {
+		metadata, err := readCSIVolumeDeviceMetadata(metadataPath)
+		if err != nil {
+			klog.Errorf("failed to read CSI block volume metadata %s: %v", metadataPath, err)
+			continue
+		}
+
+		if metadata[driverNameKey] == driverName {
+			blockVolumes++
+		}
+	}
+
+	return blockVolumes, nil
+}
+
+func readCSIVolumeDeviceMetadata(metadataPath string) (map[string]string, error) {
+	metadataBuf, err := os.ReadFile(metadataPath)
+	if err != nil {
+		return nil, fmt.Errorf("read file: %w", err)
+	}
+
+	var metadata map[string]string
+	if err := json.Unmarshal(metadataBuf, &metadata); err != nil {
+		return nil, fmt.Errorf("parse JSON: %w", err)
+	}
+
+	return metadata, nil
+}
