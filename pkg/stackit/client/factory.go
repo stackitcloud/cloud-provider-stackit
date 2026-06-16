760bbafdce32079ca82079ed3b60c6cd2d1718fa
Aniruddha Basak <aniruddha.basak@stackit.cloud>
2026-06-16 10:00:39 +0200

    remove unused projectID and add uuid in object creation in client

--- pkg/stackit/client/factory.go
+++ pkg/stackit/client/factory.go
@@ -5,7 +5,7 @@ import sdkconfig "github.com/stackitcloud/stackit-sdk-go/core/config"
 // Factory produces clients for various STACKIT services.
 type Factory interface {
 	// LoadBalancing returns a STACKIT load balancing service client.
-	LoadBalancing(ptions []sdkconfig.ConfigurationOption) (LoadBalancingClient, error)
+	LoadBalancing(options []sdkconfig.ConfigurationOption) (LoadBalancingClient, error)
 
 	// IaaS returns a STACKIT IaaS service client.
 	IaaS(options []sdkconfig.ConfigurationOption) (IaaSClient, error)
