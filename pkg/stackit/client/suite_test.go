d79bee53a3595c125ba777a61002392646930a8a
Aniruddha Basak <aniruddha.basak@stackit.cloud>
2026-07-07 16:29:00 +0200

    add tests for new stackit client

---
+++ pkg/stackit/client/suite_test.go
@@ -0,0 +1,13 @@
+package client
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestSTACKITProvider(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "CSI STACKIT Provider Suite")
+}
