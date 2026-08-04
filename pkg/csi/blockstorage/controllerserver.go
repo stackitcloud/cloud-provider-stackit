71b0721e6eb2ec425bfb0a5e7da4029c50f8d8d3
Niclas Schad <niclas.schad@stackit.cloud>
2026-08-04 12:20:54 +0200

    WIP: Delete Volume when creation fails with status ERROR
    Signed-off-by: Niclas Schad <niclas.schad@stackit.cloud>

--- pkg/csi/blockstorage/controllerserver.go
+++ pkg/csi/blockstorage/controllerserver.go
@@ -21,6 +21,7 @@ import (
 	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/container-storage-interface/spec/lib/go/csi"
@@ -265,14 +266,26 @@ func (cs *controllerServer) CreateVolume(ctx context.Context, req *csi.CreateVol
 
 	targetStatus := []string{stackitclient.VolumeAvailableStatus}
 	// Recheck after: 0s (immediate), 20s, 45.6s, 78.36s, 120.31s
-	err = cloud.WaitVolumeTargetStatusWithCustomBackoff(ctx, *vol.Id, targetStatus,
+	err = cloud.WaitVolumeTargetStatusWithCustomBackoff(ctx, &vol, targetStatus,
 		&wait.Backoff{
 			Duration: 20 * time.Second,
 			Steps:    5,
 			Factor:   1.28,
 		})
 	if err != nil {
-		klog.Errorf("Failed to WaitVolumeTargetStatus of volume %s: %v", *vol.Id, err)
+		klog.Errorf("Failed to WaitVolumeTargetStatus of volume %s: %v", vol.GetId(), err)
+
+		if err != nil {
+			klog.Errorf("Failed to fetch volume %s status during cleanup check: %v", vol.GetId(), err)
+		} else if strings.ToUpper(vol.GetStatus()) == stackitclient.VolumeErrorStatus {
+			klog.Warningf("Volume %s entered ERROR status, attempting cleanup deletion...", vol.GetId())
+			if deleteErr := cloud.DeleteVolume(ctx, vol.GetId()); deleteErr != nil {
+				klog.Errorf("Failed to delete erroneous volume %s: %v", vol.GetId(), deleteErr)
+			} else {
+				klog.Infof("Successfully deleted erroneous volume %s", vol.GetId())
+			}
+		}
+
 		return nil, status.Error(codes.Internal, fmt.Sprintf("CreateVolume Volume %s failed getting available in time: %v", *vol.Id, err))
 	}
 
