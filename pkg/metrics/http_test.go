a480cb09af41a7ef6995dfba6ae204e419341bda
stackit-ske <89182727+stackit-ske@users.noreply.github.com>
2026-08-21 15:23:26 +0200

    [release-v1.34] Refactor SDK metrics: rename metrics from `cloud_provider_stackit_http*` to `stackit_api_http.*` (#1531)
    * refactor metrics
    
    * Only use IaaS in tests, as this is used in most repos
    
    * Add PR Review suggestions
    
    ---------
    
    Co-authored-by: Maximilian Geberl <maximilian.geberl@digits.schwarz>

--- pkg/metrics/http_test.go
+++ pkg/metrics/http_test.go
@@ -1,163 +1,206 @@
 package metrics
 
 import (
+	"context"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
 
+	"github.com/google/uuid"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/testutil"
 	dto "github.com/prometheus/client_model/go"
+	sdkconfig "github.com/stackitcloud/stackit-sdk-go/core/config"
+	iaas "github.com/stackitcloud/stackit-sdk-go/services/iaas/v2api"
 )
 
 var _ = Describe("Metrics", func() {
-	DescribeTable("operationFromRequest", func(method, path, expected string) {
-		requestURL, _ := url.Parse("https://host" + path)
-		request := &http.Request{
-			Method: method,
-			URL:    requestURL,
-		}
-		op := operationFromRequest(request)
-		Expect(op).To(Equal(expected))
-	},
-		Entry("post token", "POST", "/token", "post_token"),
-		Entry("get load-balancers", "GET", "/v2/projects/6-a-4-8-c/regions/eu01/load-balancers", "get_load-balancers"),
-		Entry("get load-balancers instance", "GET", "/v2/projects/6-a-4-8-c/regions/eu01/load-balancers/id", "get_load-balancers_instance"),
-	)
+	Describe("getSDKOperationName", func() {
+		var (
+			server     *httptest.Server
+			host       string
+			component  = "test"
+			iaasClient *iaas.APIClient
+		)
 
-	Describe("InstrumentedRoundTripper", func() {
-		It("increments HTTPRequestCount for responses", func() {
-			server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+		BeforeEach(func() {
+			server = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
 				w.WriteHeader(http.StatusOK)
 			}))
-			defer server.Close()
+			url, err := url.Parse(server.URL)
+			Expect(err).NotTo(HaveOccurred())
+			host = url.Host
+
+			HTTPRequestCount.Reset()
+			HTTPErrorCount.Reset()
+			HTTPRequestDurationHistogram.Reset()
+
+			iaasClient, err = iaas.NewAPIClient(
+				sdkconfig.WithHTTPClient(NewHTTPClient(component)),
+				sdkconfig.WithEndpoint(server.URL),
+				sdkconfig.WithoutAuthentication(),
+			)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		AfterEach(func() {
+			server.Close()
+		})
+
+		It("should return DeleteVolume as operation", func() {
+			err := iaasClient.DefaultAPI.DeleteVolume(context.TODO(), uuid.New().String(), "", uuid.New().String()).Execute()
+			Expect(err).NotTo(HaveOccurred())
 
 			labels := prometheus.Labels{
-				apiLabel:       "test",
-				methodLabel:    "GET",
-				operationLabel: "get_request-count-test",
+				hostLabel:      host,
+				componentLabel: component,
+				methodLabel:    "DELETE",
+				operationLabel: "DeleteVolume",
 				codeLabel:      "200",
 			}
-			before := testutil.ToFloat64(HTTPRequestCount.With(labels))
 
-			client := NewInstrumentedHTTPClient("test")
+			Expect(testutil.ToFloat64(HTTPRequestCount.With(labels))).To(Equal(float64(1)))
+		})
 
-			response, err := client.Get(server.URL + "/request-count-test")
+		It("should return DeleteServer as operation", func() {
+			err := iaasClient.DefaultAPI.DeleteServer(context.TODO(), uuid.New().String(), "", uuid.New().String()).Execute()
+			Expect(err).NotTo(HaveOccurred())
+
+			labels := prometheus.Labels{
+				hostLabel:      host,
+				componentLabel: component,
+				methodLabel:    "DELETE",
+				operationLabel: "DeleteServer",
+				codeLabel:      "200",
+			}
+
+			Expect(testutil.ToFloat64(HTTPRequestCount.With(labels))).To(Equal(float64(1)))
+		})
+	})
+
+	Describe("InstrumentedRoundTripper", func() {
+		var (
+			server     *httptest.Server
+			httpClient *http.Client
+			host       string
+			component  = "test"
+		)
+
+		BeforeEach(func() {
+			server = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				switch r.URL.Path {
+				case "/404":
+					w.WriteHeader(http.StatusNotFound)
+				case "/500":
+					w.WriteHeader(http.StatusInternalServerError)
+				case "/400":
+					w.WriteHeader(http.StatusBadRequest)
+				default:
+					w.WriteHeader(http.StatusOK)
+				}
+			}))
+			url, err := url.Parse(server.URL)
+			Expect(err).NotTo(HaveOccurred())
+			host = url.Host
+			httpClient = NewHTTPClient(component)
+
+			HTTPRequestCount.Reset()
+			HTTPErrorCount.Reset()
+			HTTPRequestDurationHistogram.Reset()
+		})
+
+		AfterEach(func() {
+			server.Close()
+		})
+
+		It("increments HTTPRequestCount for responses", func() {
+			labels := prometheus.Labels{
+				hostLabel:      host,
+				componentLabel: component,
+				methodLabel:    "GET",
+				operationLabel: UnknownOperation,
+				codeLabel:      "200",
+			}
+
+			response, err := httpClient.Get(server.URL + "/request-count-test")
 			Expect(err).NotTo(HaveOccurred())
 			defer response.Body.Close()
 
-			after := testutil.ToFloat64(HTTPRequestCount.With(labels))
-			Expect(after - before).To(Equal(float64(1)))
+			Expect(testutil.ToFloat64(HTTPRequestCount.With(labels))).To(Equal(float64(1)))
 		})
 
 		It("records HTTPRequestDurationHistogram observations for responses", func() {
-			server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
-				w.WriteHeader(http.StatusOK)
-			}))
-			defer server.Close()
-
 			labels := prometheus.Labels{
-				apiLabel:       "test",
+				hostLabel:      host,
+				componentLabel: component,
 				methodLabel:    "GET",
-				operationLabel: "get_request-duration-test",
+				operationLabel: UnknownOperation,
 				codeLabel:      "200",
 			}
-			before := histogramSampleCount(HTTPRequestDurationHistogram.With(labels))
 
-			client := NewInstrumentedHTTPClient("test")
-
-			response, err := client.Get(server.URL + "/request-duration-test")
+			response, err := httpClient.Get(server.URL + "/request-duration-test")
 			Expect(err).NotTo(HaveOccurred())
 			defer response.Body.Close()
 
-			after := histogramSampleCount(HTTPRequestDurationHistogram.With(labels))
-			Expect(after - before).To(Equal(uint64(1)))
+			Expect(histogramSampleCount(HTTPRequestDurationHistogram.With(labels))).To(Equal(uint64(1)))
 		})
 
 		It("increments HTTPErrorCount for error responses (400, 404, 500)", func() {
-			server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				if r.Method == http.MethodPost {
-					w.WriteHeader(http.StatusInternalServerError)
-					return
-				}
-				if r.URL.Path == "/404" {
-					w.WriteHeader(http.StatusNotFound)
-					return
-				}
-				w.WriteHeader(http.StatusBadRequest)
-			}))
-			defer server.Close()
-
 			labels400 := prometheus.Labels{
-				apiLabel:       "test",
+				hostLabel:      host,
+				componentLabel: component,
 				methodLabel:    http.MethodGet,
-				operationLabel: "get_",
+				operationLabel: UnknownOperation,
 				codeLabel:      "400",
 			}
 			labels404 := prometheus.Labels{
-				apiLabel:       "test",
+				hostLabel:      host,
+				componentLabel: component,
 				methodLabel:    http.MethodGet,
-				operationLabel: "get_404",
+				operationLabel: UnknownOperation,
 				codeLabel:      "404",
 			}
 			labels500 := prometheus.Labels{
-				apiLabel:       "test",
+				hostLabel:      host,
+				componentLabel: component,
 				methodLabel:    http.MethodPost,
-				operationLabel: "post_",
+				operationLabel: UnknownOperation,
 				codeLabel:      "500",
 			}
-			before400 := testutil.ToFloat64(HTTPErrorCount.With(labels400))
-			before404 := testutil.ToFloat64(HTTPErrorCount.With(labels404))
-			before500 := testutil.ToFloat64(HTTPErrorCount.With(labels500))
 
-			client := NewInstrumentedHTTPClient("test")
-
-			response1, err := client.Get(server.URL)
+			response1, err := httpClient.Get(server.URL + "/400")
 			Expect(err).NotTo(HaveOccurred())
 			defer response1.Body.Close()
 
-			response2, err := client.Get(server.URL + "/404")
+			response2, err := httpClient.Get(server.URL + "/404")
 			Expect(err).NotTo(HaveOccurred())
 			defer response2.Body.Close()
 
-			response3, err := client.Post(server.URL, "application/json", nil)
+			response3, err := httpClient.Post(server.URL+"/500", "application/json", nil)
 			Expect(err).NotTo(HaveOccurred())
 			defer response3.Body.Close()
 
-			after400 := testutil.ToFloat64(HTTPErrorCount.With(labels400))
-			after404 := testutil.ToFloat64(HTTPErrorCount.With(labels404))
-			after500 := testutil.ToFloat64(HTTPErrorCount.With(labels500))
-
-			Expect(after400 - before400).To(Equal(float64(1)))
-			Expect(after404 - before404).To(Equal(float64(1)))
-			Expect(after500 - before500).To(Equal(float64(1)))
-			Expect((after400 - before400) + (after404 - before404) + (after500 - before500)).To(Equal(float64(3)))
+			Expect(testutil.ToFloat64(HTTPErrorCount.With(labels400))).To(Equal(float64(1)))
+			Expect(testutil.ToFloat64(HTTPErrorCount.With(labels404))).To(Equal(float64(1)))
+			Expect(testutil.ToFloat64(HTTPErrorCount.With(labels500))).To(Equal(float64(1)))
 		})
 
 		It("does not increment HTTPErrorCount for successful responses", func() {
-			server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
-				w.WriteHeader(http.StatusOK)
-			}))
-			defer server.Close()
-
 			labels := prometheus.Labels{
-				apiLabel:       "test",
+				hostLabel:      host,
+				componentLabel: component,
 				methodLabel:    http.MethodGet,
-				operationLabel: "get_",
+				operationLabel: UnknownOperation,
 				codeLabel:      "200",
 			}
-			before := testutil.ToFloat64(HTTPErrorCount.With(labels))
 
-			client := NewInstrumentedHTTPClient("test")
-
-			response, err := client.Get(server.URL)
+			response, err := httpClient.Get(server.URL)
 			Expect(err).NotTo(HaveOccurred())
 			defer response.Body.Close()
 
-			after := testutil.ToFloat64(HTTPErrorCount.With(labels))
-			Expect(after - before).To(Equal(float64(0)))
+			Expect(testutil.ToFloat64(HTTPErrorCount.With(labels))).To(Equal(float64(0)))
 		})
 	})
 })
