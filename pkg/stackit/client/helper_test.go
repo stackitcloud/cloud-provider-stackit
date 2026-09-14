6a82987eeb834746b0ec73df0bc44380e9a9895a
whtssub <subhasmitaswain232@gmail.com>
2026-09-14 16:46:35 +0200

    Add traceID in error logging of stackit api calls
    formatting fix
    
    update helper func name to generic

---
+++ pkg/stackit/client/helper_test.go
@@ -0,0 +1,27 @@
+package client
+
+import (
+	"context"
+	"errors"
+	"net/http"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	sdkconfig "github.com/stackitcloud/stackit-sdk-go/core/config"
+)
+
+var _ = Describe("execute", func() {
+	It("wraps API errors with trace and request IDs", func() {
+		_, err := execute(context.Background(), func(ctx context.Context) (int, error) {
+			response, ok := ctx.Value(sdkconfig.ContextHTTPResponse).(**http.Response)
+			Expect(ok).To(BeTrue())
+			*response = &http.Response{Header: http.Header{
+				"X-Trace-Id":   {"trace-123"},
+				"X-Request-Id": {"request-456"},
+			}}
+			return 0, errors.New("api error")
+		})
+
+		Expect(err).To(MatchError("[X-Request-Id:request-456]: [X-Trace-Id:trace-123]: api error"))
+	})
+})
